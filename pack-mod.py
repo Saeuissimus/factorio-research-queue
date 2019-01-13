cba31ee191d218e90364306477f1a17aada5a725
Saeuissimus <nalus_saeuus@protonmail.com>
2019-01-13 10:33:30 -0300

    Some small improvements to the packing script.

--- pack-mod.py
+++ pack-mod.py
@@ -6,15 +6,15 @@ import zipfile, os, sys
 archive_root = "research-queue_1.6.8"
 
 
-with zipfile.ZipFile(archive_root + ".zip", "w") as archive:
+with zipfile.ZipFile(archive_root + ".zip", "w", compression=zipfile.ZIP_LZMA) as archive:
   for root, dirs, files in os.walk("."):
     # print("Root: " + str(root) + " Dirs: " + str(dirs) + " Files: " + str(files))
     if ".git" in dirs:
       dirs.remove(".git")
     for file in files:
-      if file == archive_root + ".zip":
-        continue
       file_path = os.path.normpath(os.path.join(root, file))
+      if not os.path.isfile(file_path) or ".zip" in file or file in __file__:
+        continue
       archive_path = os.path.normpath(os.path.join(archive_root, root, file))
       print("File path: " + str(file_path) + " Archive path: " + str(archive_path))
       archive.write(file_path, archive_path)
