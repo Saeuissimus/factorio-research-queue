61fd56cac52427d655aed9d5c6d2b111c22e46d3
Saeuissimus <nalus_saeuus@protonmail.com>
2019-03-03 15:23:33 -0300

    Modified pack-mod.py to read version from info.json.

--- pack-mod.py
+++ pack-mod.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-import zipfile, os
+import zipfile, os, json
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+info_path = os.path.join(base_dir, "info.json")
 
-archive_root = "research-queue-experimental_1.6.8"
+with open(info_path) as info_file:
+  info = json.load(info_file)
+  version = info["version"]
 
+archive_root = "research-queue-experimental_" + version
+archive_name = os.path.join(base_dir, archive_root + ".zip")
 
-with zipfile.ZipFile(archive_root + ".zip", "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as archive:
-  for root, dirs, files in os.walk("."):
+with zipfile.ZipFile(archive_name, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as archive:
+  for root, dirs, files in os.walk(base_dir):
     # print("Root: " + str(root) + " Dirs: " + str(dirs) + " Files: " + str(files))
     if ".git" in dirs:
       dirs.remove(".git")
@@ -15,7 +21,7 @@ with zipfile.ZipFile(archive_root + ".zip", "w", compression=zipfile.ZIP_DEFLATE
       file_path = os.path.normpath(os.path.join(root, file))
       if not os.path.isfile(file_path) or ".zip" in file or file in __file__:
         continue
-      archive_path = os.path.normpath(os.path.join(archive_root, root, file))
+      archive_path = os.path.normpath(os.path.join(archive_root, os.path.relpath(os.path.join(root, file), start=base_dir)))
       print("File path: " + str(file_path) + " Archive path: " + str(archive_path))
       archive.write(file_path, archive_path)
 
